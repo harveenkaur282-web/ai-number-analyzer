3783f3e52d2498dfd76daa495f3f809397258258
Harveen Kaur <harveenkaur282@gmail.com>
2026-04-08 18:01:56 +0000

    adding social engine but the numbers might class with meme engine: need to work on same, just designed minimal architecture

---
+++ backend/engines/social_engine.py
@@ -0,0 +1,9 @@
+def analyze(n: int):
+    if n == 3:
+        return "Group project member who did nothing 😭"
+    elif n == 100:
+        return "Topper. No sleep. No social life."
+    elif n == 69:
+        return "Class giggling detected."
+    else:
+        return "Social status: irrelevant but observed 👀"
